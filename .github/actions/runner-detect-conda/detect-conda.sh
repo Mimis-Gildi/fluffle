a9ba15d984fb27f0a92c2dd0e5ad8c6b105a65f6
R!dd13r <12781006+rdd13r@users.noreply.github.com>
2026-04-07 18:33:24 -0400

    Testing Conda Canary

--- .github/actions/runner-detect-conda/detect-conda.sh
+++ .github/actions/runner-detect-conda/detect-conda.sh
@@ -15,5 +15,5 @@ readonly mamba_version_output=$(mamba  --version 2>/dev/null) || exit 0  # 2.3.3
 readonly py_ml_actual=$(echo "$py_ml_version_output" | awk '/^Python/ { print $2 }')
 readonly conda_actual=$(echo "$conda_version_output" | awk '/^conda/ { print $2 }')
 
-printf '::warning title=Python Versions::py=<%s.%s>, co=<%s.%s>, ma=<%s>\n' "$py_ml_version" "$py_ml_actual" "$conda_version_output" "$conda_actual" "$mamba_version_output"
+printf '::warning title=Python Versions::py=<%s-%s>, co=<%s-%s>, ma=<%s>\n' "$py_ml_version_output" "$py_ml_actual" "$conda_version_output" "$conda_actual" "$mamba_version_output"
 
