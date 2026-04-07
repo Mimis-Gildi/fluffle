e82b40f9dda339e0096e62abaef03abb282e20f1
R!dd13r <12781006+rdd13r@users.noreply.github.com>
2026-04-07 18:40:02 -0400

    Testing Conda Canary

--- .github/actions/runner-detect-conda/detect-conda.sh
+++ .github/actions/runner-detect-conda/detect-conda.sh
@@ -4,16 +4,15 @@ readonly py_ml_version="$1"
 readonly conda_version="$2"
 readonly mamba_version="$3"
 
-source "${0:A:h}/conda-activate.sh"
+readonly activation="${0:A:h}/conda-activate.sh"
+[[ -s "$activation" ]] && source "$activation"
 
-conda activate ml
+conda activate ml || exit 0
 
-readonly py_ml_version_output=$(python --version 2>/dev/null) || exit 0  # Python 3.12.13
-readonly conda_version_output=$(conda  --version 2>/dev/null) || exit 0  # conda 26.1.1
-readonly mamba_version_output=$(mamba  --version 2>/dev/null) || exit 0  # 2.3.3
+readonly py_ml_version=$(python --version 2>/dev/null | awk '/^Python/ { print $2 }') || exit 0  # Python 3.12.13
+readonly conda_version=$(conda  --version 2>/dev/null  | awk '/^conda/ { print $2 }') || exit 0  # conda 26.1.1
+readonly mamba_version=$(mamba  --version 2>/dev/null) || exit 0  # 2.3.3
 
-readonly py_ml_actual=$(echo "$py_ml_version_output" | awk '/^Python/ { print $2 }')
-readonly conda_actual=$(echo "$conda_version_output" | awk '/^conda/ { print $2 }')
 
-printf '::warning title=Python Versions::py=<%s-%s>, co=<%s-%s>, ma=<%s>\n' "$py_ml_version_output" "$py_ml_actual" "$conda_version_output" "$conda_actual" "$mamba_version_output"
+printf '::warning title=Python Versions::py_ml=<%s>, conda=<%s>, mamba=<%s>\n' "$py_ml_version" "$conda_version" "$mamba_version"
 
