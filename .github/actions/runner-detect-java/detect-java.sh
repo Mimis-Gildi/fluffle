a731cd25e20a24b0abcc2a5689c8f8fcb9ed3a4f
R!dd13r <12781006+rdd13r@users.noreply.github.com>
2026-04-08 03:16:18 -0400

    Testing Java Canary Upgrade

--- .github/actions/runner-detect-java/detect-java.sh
+++ .github/actions/runner-detect-java/detect-java.sh
@@ -10,9 +10,7 @@ sdk flush version
 readonly actual=$(sdk current java 2>/dev/null | awk '{ print $NF }') || exit 0
 readonly latest=$(sdk list java 2>/dev/null | grep -oE '21\.[0-9.]+\-tem' | sort -V | tail -n1) || exit 0
 
-printf '<==| State of Java: %s -> %s.\n' "$latest" "$actual"
-
 if [[ "$actual" != "$latest" ]]; then
-  printf '::warning title=Java outdated::active %s, latest 21-tem is %s\n' "$actual" "$latest"
+  printf '::warning title=Java outdated::active is %s while latest is %s\n' "$actual" "$latest"
   echo "failed=true" > "$GITHUB_OUTPUT"
 fi
