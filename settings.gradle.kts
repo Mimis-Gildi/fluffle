18caf6d44cd02c854c58203ab2763d13ee5951fa
R!dd13r <12781006+rdd13r@users.noreply.github.com>
2026-04-09 05:08:28 -0400

    Claude was here!

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,5 @@
-/* rdd13r 2025-11-14
-Mimis v3
-*/
-
 pluginManagement {
+
     val versionOfToolchainsFoojayResolver: String by extra
 
     repositories {
@@ -13,9 +10,6 @@ pluginManagement {
     plugins {
         id("org.gradle.toolchains.foojay-resolver-convention") version versionOfToolchainsFoojayResolver
     }
-
-    includeBuild("build-logic")
 }
 
 rootProject.name = "fluffle"
-include("app", "list", "utilities")
